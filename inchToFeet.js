1cd71096ad63d4bed091ba94108b010a41a93d06
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-06 01:44:50 -0700

    inch to feet convator

---
+++ inchToFeet.js
@@ -0,0 +1,36 @@
+// inch to fet 
+function inchToFeet(inches) {
+    const feet = inches / 12;
+    return feet;
+}
+const feet = inchToFeet(24);
+console.log(feet);
+
+
+// 2. second
+
+function inchesToFeet(inches) {
+    return inches / 12;
+  }
+
+const inches = 24;
+const feet = inchesToFeet(inches);
+console.log(feet); 
+
+
+// 01. practice ----------
+function inchesToFeet(inches) {
+  return inches / 12;
+}
+const inches = 24;
+const feet = inchToFeet(inches);
+console.log(feet);
+
+
+// 02. practice ----------
+function inchToFeet(inches) {
+  return inches / 12;
+}
+const inches = 24;
+const feet = inchToFeet(inches);
+console.log(feet);

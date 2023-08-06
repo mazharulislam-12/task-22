9d749535326d5f67090d7b2afd175bad4b07e6d7
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-06 03:45:51 -0700

    centimeters to meters

---
+++ centimetersToMeters.js
@@ -0,0 +1,22 @@
+function centimetersToMeters(centimeters) {
+    return centimeters / 100;
+}
+const centimeters = 250;
+const meters = centimetersToMeters(centimeters);
+console.log(meters);
+
+// 01. practice ----------
+function centimetersToMeters(centimeters) {
+    return centimeters / 100;
+}
+const centimeters = 200;
+const meters = centimetersToMeters(centimeters);
+console.log(meters);
+
+// 02. practice ------------
+function centimetersToMeters(centimeters) {
+    return centimeters / 100;
+}
+const centimeters = 500;
+const meters = centimetersToMeters(centimeters);
+console.log(meters);

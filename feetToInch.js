c8685c5d55b211b3902f0a36411465ec0f4da327
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-06 03:35:12 -0700

    feet to incch convetor

---
+++ feetToInch.js
@@ -0,0 +1,23 @@
+function feetToInch(feet) {
+    return feet * 12;
+}
+const feet = 1;
+const inches = feetToInch(feet);
+console.log(inches);
+
+
+// 01. practice ---------
+function feetToInch(feet) {
+    return feet * 12;
+}
+const feet = 5;
+const inches = feetToInch(feet);
+console.log(inches);
+
+// 02. practice ----------
+function feetToInch(feet) {
+    return feet * 12;
+}
+const feet = 5;
+const inches = feetToInch(feet);
+console.log(inches);

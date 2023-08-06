6cb5abb551dd301b4363944c6590e5a1b36487c2
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-06 01:58:27 -0700

    even or odd number check function

---
+++ evenAndOddCheck.js
@@ -0,0 +1,36 @@
+function isEvenOrOdd(number) {
+    if (number % 2 === 0 ) {
+        console.log("even", number);
+    }
+    else{
+        console.log("Odd", number);
+    }
+}
+console.log(isEvenOrOdd(4));
+console.log(isEvenOrOdd(5));
+
+// 01. practice-----------
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log("even", number);
+    }
+    else{
+        console.log("Odd", number);
+    }
+}
+console.log(isEvenOrOdd(4));
+console.log(isEvenOrOdd(5));
+console.log(isEvenOrOdd(6));
+
+
+// 02. practice --------
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log("even", number);
+    }
+    else{
+        console.log("odd", number);
+    }
+}
+console.log(isEvenOrOdd(5));
+console.log(isEvenOrOdd(60));

78790fd20b122b53b682ac1f16baee38e30a1135
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-06 03:27:21 -0700

    factorial

---
+++ factorial.js
@@ -0,0 +1,39 @@
+function factorial(number) {
+    let result = 1;
+    for(let i = number; i >= 1; i--){
+        result = result * i;
+        console.log(i);
+    }
+    return result;
+}
+const number = 5;
+const fact = factorial(number);
+console.log("Factorial of",number, fact);
+
+
+// 01. practice ------
+function factorial(number) {
+    let result = 1;
+    for(let i = number; i >= 1; i--){
+        result * i;
+        // console.log(i);
+    }
+    return result;
+}
+const number = 5;
+const fact = factorial(number)
+console.log(fact);
+
+
+// 02. practice ------
+function factorial(number) {
+    let result = 1;
+    for(let i = number; i >= 1; i--){
+        result * i;
+        console.log(i);
+    }
+    return result;
+}
+const number = 5;
+const fact = factorial(number);
+console.log(fact);
